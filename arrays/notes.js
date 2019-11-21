21f256caca46a614a841b44f827ccc17e4a0eecf
tmax818 <tylermaxwell661@gmail.com>
2019-11-21 12:56:23 -0800

    asdf

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,5 +1,35 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [
+    {
+        title: "My next trip",
+        body: "I would like to go to Spain"
+    }, {
+        title: "Habbits to work on",
+        body: "Exercise, coding structure"
+    }, {
+        title: "each other",
+        body: "sothing goes here later"
+    }
+
+]
+
+let poped = notes.pop() // removes an item from the end
+notes.push('My new pushed note')
+
+//A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
+
+notes.forEach(function (item, index){
+    console.log(index, item)
+})  
+
 
 console.log(notes)
 console.log(notes.length)
+console.log(poped)
+
+//Counting
+
+for(let i = 0; i <= 100; i++){
+    console.log(i)
+
+}
 
